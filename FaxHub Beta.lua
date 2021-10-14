57dd1ae57bb959a208870f3fd832a82c3cb5a97c
ScriptedBoxxed <92398214+ScriptedBoxxed@users.noreply.github.com>
2021-10-14 10:07:17 -0500

    Create FaxHub Beta.lua
    This is to test Kavo Ui Library Features

---
+++ FaxHub Beta.lua
@@ -0,0 +1,23 @@
+if game.PlaceID == 
+  then
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("FaxHub Beta", "Synapse")
+
+local Tab = Window:NewTab("Main")
+local MainSection = Tab:NewSection("Main")
+MainSection:NewLabel("Ui Toggle")  
+end)
+MainSection:NewKeybind("Toggle GUI With F", "Toggles the GUI when F is pressed", Enum.KeyCode.F, function()	Library:ToggleUI()
+end)
+  
+
+    end
+end)
+
+
+local Tab = Window:NewTab("Player")
+local PlayerSection = Tab:NewSection("Player")  
+
+end)
+
